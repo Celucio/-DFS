33db9d11f5dfae46cf1cb52ca31da9358090652f
Celucio <75482093+Celucio@users.noreply.github.com>
2022-06-02 19:46:00 -0500

    Primer commit - Importación de la Libreria Queue

---
+++ [NRC_6278]_CarlosLucio_Lab3Unidad1.py
@@ -0,0 +1,9 @@
+'''
+Autor: Carlos Lucio
+Para comenzar con la generación de los métodos de busqueda necesitaremos de las librerias necesarias
+En python los metodos de busqueda se puede asociar con la teoria de Grafos.
+Es por ello que para crear la clase Grafos
+Importaremos la libreria Queue para generar listas en el programa
+'''
+#Importamos la libreria Queue
+from queue import Queue
